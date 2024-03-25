37e41b66fd73aed719f711318d408aca1ab3d45f
sampath kumar <134235018+SampaTH991207@users.noreply.github.com>
2024-03-25 18:14:00 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,8 +6,8 @@ sonarqubeScanner(
     options: {
       'sonar.projectDescription': 'This is a Node JS application',
       'sonar.projectName': 'Node.JS Application - Sample',
-      'sonar.projectKey': 'NodeJsMithunTechnologies',
-      'sonar.login': 'ecdc55c449fc4b36767898f4b19d19a5bff890f6',
+      'sonar.projectKey': 'NodeJsMss',
+      'sonar.login': 'b7e1f0ae22dae09aa1d775d97b0d56ab0e6b54a6',
       'sonar.projectVersion': '1.0',
       'sonar.language': 'js',
       'sonar.sourceEncoding': 'UTF-8',
