b303563f8f1b6dad56c31a2aabc6829bd645a460
sampath kumar <134235018+SampaTH991207@users.noreply.github.com>
2024-03-25 17:05:28 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,20 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
-sonarqubeScanner({
-  serverUrl: 'http://18.221.74.112:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
-}, () => {});
+sonarqubeScanner(
+  {
+    serverUrl: 'https://sonarcloud.io/',
+    options: {
+      'sonar.projectDescription': 'This is a Node.js application',
+      'sonar.projectName': 'Node.js Application - Sample',
+      'sonar.projectKey': 'NodeJsMSS',
+      'sonar.login': 'ecdc55c449fc4b36767898f4b19d19a5bff890f6',
+      'sonar.projectVersion': '1.0',
+      'sonar.language': 'js',
+      'sonar.sourceEncoding': 'UTF-8',
+      'sonar.sources': '.',
+      //'sonar.tests': 'specs',
+      //'sonar.inclusions': 'src/**'
+    },
+  },
+  () => {}
+);
